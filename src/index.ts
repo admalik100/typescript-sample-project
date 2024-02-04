bec50dd954f078fdcdbc9c5826f62e5e3296b2f9
admalik100 <ahmad.amir.malick@gmail.com>
2024-02-04 19:05:18 +0500

    explore the any type in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-let age: number = 20;
+// in Typescript a large number's digits can be separated via adding a underscore for better readability
+let sales: number = 123_456_789;
+let course: string = 'typescript';
+let isPublished: boolean = true;
+// if a variable is not initialized and has no type defined -> Typescript maps it to the "any" type
+// should be avoided since it can be assigned values same as in JS (no type check)
+let level;
+level = 1;
+level = 'a';
 
-if (age < 50) {
-  age += 10;
+function render(document: any) {
+  console.log(document);
 }
-console.log(age);
