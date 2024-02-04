4687a1143b14a9280290f1279d86ea18d3256632
admalik100 <ahmad.amir.malick@gmail.com>
2024-02-04 19:50:58 +0500

    objects and type aliasing

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,28 @@ function render(document) {
 }
 let numbers = [1, 2, 3];
 let user = [1, 'Mosh'];
-let mySize = 4;
+var size;
+(function (size) {
+    size[size["small"] = 2] = "small";
+    size[size["medium"] = 4] = "medium";
+    size[size["large"] = 6] = "large";
+})(size || (size = {}));
+let mySize = size.medium;
 console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000);
+calculateTax(10000, 2023);
+let employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee.name = 'Mosh';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -61,3 +61,19 @@ function calculateTax(income: number, taxYear: number = 2022): number {
 
 calculateTax(10_000);
 calculateTax(10_000, 2023);
+
+// objects
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+let employee: Employee = {
+  id: 1,
+  name: 'Mosh',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+employee.name = 'Mosh';
