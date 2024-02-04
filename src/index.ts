614e1be12a868248e541876abdb5693ddf4eb559
admalik100 <ahmad.amir.malick@gmail.com>
2024-02-04 19:41:04 +0500

    all about functions

--- src/index.ts
+++ src/index.ts
@@ -42,3 +42,22 @@ enum size {
 
 let mySize: size = size.medium;
 console.log(mySize);
+
+// functions
+
+// function calculateTax(income: number, taxYear?: number): number {
+//   if ((taxYear || 2022) < 2022) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
+// }
+
+function calculateTax(income: number, taxYear: number = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calculateTax(10_000);
+calculateTax(10_000, 2023);
