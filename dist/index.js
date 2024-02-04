a2182dd154e056660c408f52816499d370e974a9
Ahmad Malik <ahmad.amir.malick@gmail.com>
2024-02-04 23:29:33 +0500

    Tsc practice (#1)
    * ts compiler config and prettier config
    
    * debugging the files
    
    * explore the any type in typescript
    
    * arrays and enums
    
    * all about functions
    
    * objects and type aliasing
    
    * union types

---
+++ dist/index.js
@@ -0,0 +1,45 @@
+"use strict";
+let sales = 123456789;
+let course = 'typescript';
+let isPublished = true;
+let level;
+level = 1;
+level = 'a';
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let user = [1, 'Mosh'];
+var size;
+(function (size) {
+    size[size["small"] = 2] = "small";
+    size[size["medium"] = 4] = "medium";
+    size[size["large"] = 6] = "large";
+})(size || (size = {}));
+let mySize = size.medium;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000);
+calculateTax(10000, 2023);
+let employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee.name = 'Mosh';
+function convertKgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseFloat(weight) * 2.2;
+    }
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+// in Typescript a large number's digits can be separated via adding a underscore for better readability
+let sales: number = 123_456_789;
+let course: string = 'typescript';
+let isPublished: boolean = true;
+// if a variable is not initialized and has no type defined -> Typescript maps it to the "any" type
+// should be avoided since it can be assigned values same as in JS (no type check)
+let level;
+level = 1;
+level = 'a';
+
+function render(document: any) {
+  console.log(document);
+}
+
+// --------Arrays
+
+let numbers: number[] = [1, 2, 3];
+
+// ----------Tuples
+// can have multi type data in it -> Use case store a user who has an id and a name
+// 1, 'Mosh'
+
+let user: [number, string] = [1, 'Mosh'];
+
+// ------=Enums
+// const small: number = 1;
+// const medium: number = 2;
+// const large: number = 3;
+
+// another way is by enum -> By default TS sets values to them as 0 , 1 ,2 , we can explicity specify values though
+enum size {
+  small = 2,
+  medium = 4,
+  large = 6,
+}
+
+// const enum size {
+//     small = 2,
+//     medium = 4,
+//     large = 6,
+//   }
+
+let mySize: size = size.medium;
+console.log(mySize);
+
+// functions
+
+// function calculateTax(income: number, taxYear?: number): number {
+//   if ((taxYear || 2022) < 2022) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
+// }
+
+function calculateTax(income: number, taxYear: number = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calculateTax(10_000);
+calculateTax(10_000, 2023);
+
+// objects
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+let employee: Employee = {
+  id: 1,
+  name: 'Mosh',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+employee.name = 'Mosh';
+
+// union types
+
+function convertKgToLbs(weight: number | string): number {
+  // narrowing technique
+  if (typeof weight === 'number') {
+    return weight * 2.2;
+  } else {
+    return parseFloat(weight) * 2.2;
+  }
+}
